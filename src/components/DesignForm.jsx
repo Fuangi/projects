53872530f06b2f1954b46965c65916da552a9064
clintjeff2 <clintjeff2@gmail.com>
2024-03-10 17:59:05 +0100

    Added code to edit and send design projects details to the database using axios

--- src/components/DesignForm.jsx
+++ src/components/DesignForm.jsx
@@ -1,46 +1,69 @@
-import { useState } from "react";
+import { useState } from 'react';
+import axios from 'axios';
 
 function DesignForm() {
-  const [members, setMembers] = useState([]);
-  const [topic, setTopic] = useState("");
+	const [members, setMembers] = useState([]);
+	const [topic, setTopic] = useState('');
+	const [showSuccess, setShowSuccess] = useState('');
 
-  function handleAddDesignProject(e) {
-    e.preventDefault();
-  }
+	function handleAddDesignProject(e) {
+		e.preventDefault();
+		console.log(members);
+		let arrayStud = members.split('\n');
 
-  return (
-    <form id="form" action="" method="post">
-      <div className="input-field">
-        <label htmlFor="name">Group member names</label>
-        <textarea
-          class="input"
-          id="textarea"
-          cols="30"
-          rows="6"
-          value={members}
-          onChange={(e) => setMembers(e.target.value)}
-        ></textarea>
-      </div>
+		axios({
+			url: 'http://localhost:8000/projects/design',
+			method: 'post',
+			data: {
+				members: arrayStud,
+				project: topic,
+			},
+		})
+			.then(function (res) {
+				if (res.status === 201) {
+					console.log(res);
+					setShowSuccess('Success!!!');
+				}
+			})
+			.catch(function (res) {
+				setShowSuccess('Failed!!! Re-submit the form');
+				console.log(res);
+			});
+	}
 
-      <div className="input-field">
-        <label htmlFor="topic">Project Topic </label>
-        <input
-          class="input"
-          type="text"
-          name="topic"
-          id="topic"
-          value={topic}
-          onChange={(e) => setTopic(e.target.value)}
-        />
-      </div>
+	return (
+		<form id="form" action="" method="post">
+			<div className="input-field">
+				<label htmlFor="name">Group member names</label>
+				<textarea
+					class="input"
+					id="textarea"
+					cols="30"
+					rows="6"
+					value={members}
+					onChange={(e) => setMembers(e.target.value)}
+				></textarea>
+			</div>
 
-      <div class="btns">
-        <button type="submit" class="btn" onClick={handleAddDesignProject}>
-          SUBMIT
-        </button>
-      </div>
-    </form>
-  );
+			<div className="input-field">
+				<label htmlFor="topic">Project Topic </label>
+				<input
+					class="input"
+					type="text"
+					name="topic"
+					id="topic"
+					value={topic}
+					onChange={(e) => setTopic(e.target.value)}
+				/>
+			</div>
+			<div className="success">{showSuccess}</div>
+			<div class="btns">
+				<button type="submit" class="btn" onClick={handleAddDesignProject}>
+					SUBMIT
+				</button>
+			</div>
+		</form>
+	);
 }
 
 export default DesignForm;
